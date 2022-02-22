97629d4492385f6464be719f1efea44a8b8d0e80
SamiulKabir <samkabir26@gmail.com>
2022-02-22 13:10:37 +0600

    posts a reminder to database

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 var cron = require('node-cron');
 const { MongoClient } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors');
 const app = express();
 
 //Setting up PORT
 const port = 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 //Connecting to Mongo DB
 const uri = `mongodb+srv://${'tweetsyuser'}:${'QCD2AzI2HM7nTDpP'}@cluster0.qmw5x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -16,23 +20,25 @@ async function run(){
         await client.connect();
         console.log('database connected!')
         const database = client.db('tweetsy');
-        const reminders = database.collection('reminders');
+        const remindersCollection = database.collection('reminders');
 
 
         //Posting a reimnder
-        app.post('/reminder', async (req, res) => {
+        app.post('/reminders', async (req, res) => {
             const reminderOne = req.body;
-            const result = await reminders.insertOne(reminderOne);
+            const result = await remindersCollection.insertOne(reminderOne).then( () => {
+                console.log("data inserted");
+            });
             res.json(result);
 
-            //initilizing the reminder
-            var task = cron.schedule('0 1 * * *', () => {
-                console.log(`Running a job at ${req.body.time} at ${req.body.timezone}`);
-            }, {
-                scheduled: true,
-                timezone: req.body.timezone
-            });
-            task.destroy();
+            // //initilizing the reminder
+            // var task = cron.schedule('0 1 * * *', () => {
+            //     console.log(`Running a reminder at ${req.body.time} at ${req.body.timezone}`);
+            // }, {
+            //     scheduled: true,
+            //     timezone: req.body.timezone
+            // });
+            // task.destroy();
         });
 
         
