924766ffea420d4b3c9cb82a775b9f41aeaf5056
samkabir <samkabir26@gmail.com>
2022-02-22 21:46:05 +0600

    all done

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 var cron = require('node-cron');
 const { MongoClient } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors');
 const app = express();
 
 //Setting up PORT
 const port = 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 //Connecting to Mongo DB
 const uri = `mongodb+srv://${'tweetsyuser'}:${'QCD2AzI2HM7nTDpP'}@cluster0.qmw5x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -16,28 +20,41 @@ async function run(){
         await client.connect();
         console.log('database connected!')
         const database = client.db('tweetsy');
-        const reminders = database.collection('reminders');
+        const remindersCollection = database.collection('reminders');
 
 
         //Posting a reimnder
-        app.post('/reminder', async (req, res) => {
+        app.post('/reminders', async (req, res) => {
+
             const reminderOne = req.body;
-            const result = await reminders.insertOne(reminderOne);
-            res.json(result);
-
-            //initilizing the reminder
-            var task = cron.schedule('0 1 * * *', () => {
-                console.log(`Running a job at ${req.body.time} at ${req.body.timezone}`);
-            }, {
-                scheduled: true,
-                timezone: req.body.timezone
+            console.log(reminderOne);
+            const result = await remindersCollection.insertOne(reminderOne).then( () => {
+                console.log("data inserted");
             });
-            task.destroy();
+            
+            //splitting the time
+            // Example: time ='14:03' 
+            let hour = reminderOne.time.substring(0,2);
+            let minute = reminderOne.time.substring(3,5);
+            console.log(hour, minute);
+
+            //splitting the day
+            // Example: date ='2022-03-24' 
+            let day = reminderOne.date.substring(8,10);
+            let month = reminderOne.date.substring(6,7);
+            console.log(day, month);
+
+            //Initilizing the reminder
+            var task = cron.schedule(`${minute} ${hour} ${day} ${month} *`, () => {
+                console.log(`Running a reminder at ${reminderOne.time} at ${reminderOne.timezone}`);
+            }, {
+                scheduled: false,
+                // timezone: reminderOne.timezone
+              });
+            task.start();
+
+            console.log('task has been initialised');
         });
-
-        
-
-
 
     }
     finally{
